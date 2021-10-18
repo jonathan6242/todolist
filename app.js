c5c92f3e8d2b451e8c60946759957e6a5e100435
jonathan6242 <89970273+jonathan6242@users.noreply.github.com>
2021-10-18 18:17:07 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -1,117 +1,48 @@
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('ul.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('ul.collection')
+const form = document.querySelector('#task-form');
+const clearButton = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
 
-loadEventListeners();
+form.addEventListener('submit', addTask)
+document.body.addEventListener('click', removeTask)
+clearButton.addEventListener('click', clearTasks)
+filter.addEventListener('keyup', filterTasks);
+window.addEventListener('DOMContentLoaded', loadTasksFromLocalStorage)
 
-function loadEventListeners() {
-    document.addEventListener('DOMContentLoaded', getTasks);
-    form.addEventListener('submit', addTask)
-    taskList.addEventListener('click', removeTask)
-    clearBtn.addEventListener('click', clearTasks)
-    filter.addEventListener('keyup', filterTasks)
-}
-
-function removeTaskFromLocalStorage(taskItem) {
-    let tasks = [];
-
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
+function loadTasksFromLocalStorage() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks !== null) {
+        tasks.forEach(function(task) {
+            const li = document.createElement('li');
+            li.className = 'collection-item';
+            li.appendChild(document.createTextNode(task));
+            const link = document.createElement('a');
+            link.innerHTML = `<i class="fa fa-remove"></i>`;
+            li.appendChild(link);
+            taskList.appendChild(li);
+        })
     }
-
-    tasks.forEach(function(task, index) {
-        if (taskItem.textContent === task) {
-            tasks.splice(index, 1);
-        }
-    })
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-function getTasks() {
-    let tasks = [];
-
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-
-    tasks.forEach(function(task) {
-        const li = document.createElement('li');
-        li.className = 'collection-item';
-        li.appendChild(document.createTextNode(task));
-        const link = document.createElement('a');
-        link.className = 'delete-item';
-        link.innerHTML = `<i class="fa fa-remove"></i>`;
-        li.appendChild(link);
-        taskList.appendChild(li);
-    })
 }
 
 function addTask(e) {
-    if(taskInput.value === '') {
-        alert('Add a task')
-    } else {
+    e.preventDefault();
+
+    if (taskInput.value.trim() !== '') {
         const li = document.createElement('li');
         li.className = 'collection-item';
-        li.style.transition = 'all 300ms ease';
-        li.style.opacity = 1;
         li.appendChild(document.createTextNode(taskInput.value));
         const link = document.createElement('a');
-        link.className = 'delete-item';
         link.innerHTML = `<i class="fa fa-remove"></i>`;
         li.appendChild(link);
         taskList.appendChild(li);
-        storeTaskInLocalStorage(taskInput.value);
+        addTaskToLocalStorage(taskInput.value);
         taskInput.value = '';
     }
-    e.preventDefault();
 }
 
-function removeTask(e) {
-    if (e.target.parentElement.classList.contains("delete-item")) {
-        e.target.parentElement.parentElement.style.transition = 'all 300ms ease';
-        e.target.parentElement.parentElement.remove();
-
-        removeTaskFromLocalStorage(e.target.parentElement.parentElement);
-    }
-    
-}
-
-function clearTasks(e) {
-    while(taskList.firstChild) {
-        taskList.removeChild(taskList.firstChild);
-    }
-
-    clearTasksFromLocalStorage();
-    e.preventDefault();
-}
-
-function clearTasksFromLocalStorage() {
-    localStorage.clear();
-}
-
-function filterTasks(e) {
-    const text = e.target.value.toLowerCase();
-
-    document.querySelectorAll('.collection-item').forEach(function(task) {
-        const item = task.firstChild.textContent;
-        if (item.toLowerCase().indexOf(text) != -1) {
-            task.style.display = 'flex';
-        } else {
-            task.style.display = 'none';
-        }
-    })
-}
-
-function storeTaskInLocalStorage(task) {
-    let tasks = [];
-    console.log(task);
+function addTaskToLocalStorage(task) {
+    let tasks;
 
     if (localStorage.getItem('tasks') === null) {
         tasks = [];
@@ -122,4 +53,54 @@ function storeTaskInLocalStorage(task) {
     tasks.push(task);
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function removeTask(e) {
+    if(e.target.className === 'fa fa-remove') {
+        removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    let taskRemoved = false;
+
+    for(let i = tasks.length - 1; i >= 0; i--) {
+        const task = tasks[i]
+        if (taskItem.textContent === task && taskRemoved === false) {
+            tasks.splice(i, 1)
+            taskRemoved = true;
+        }
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function clearTasks(e) {
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+    localStorage.clear();
+    e.preventDefault();
+}
+
+function filterTasks(e) {
+    const filter = e.target.value.toLowerCase();
+    console.log(filter);
+    Array.from(taskList.children).forEach(function(task) {
+        const taskText = task.textContent.toLowerCase();
+        if (taskText.indexOf(filter) !== -1) {
+            task.style.display = 'flex';
+        } else {
+            task.style.display = 'none';
+        }
+    })
 }
